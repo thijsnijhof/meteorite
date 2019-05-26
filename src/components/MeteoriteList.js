fe8711c3e581594257a08bf3bd11cbd0ffab4bd5
Thijs Nijhof <tnijhof91@gmail.com>
2019-05-26 21:55:33 +0100

    pagination

--- src/components/MeteoriteList.js
+++ src/components/MeteoriteList.js
@@ -2,30 +2,41 @@ import React from "react";
 import MeteoriteItem from "./MeteoriteItem";
 import MeteoriteListHead from "./MeteoriteListHead";
 
-const MeteoriteList = ({ meteorites }) => {
+const MeteoriteList = ({ meteorites, displayError }) => {
   return (
-    <div style={{overflowX:"auto"}}>
-      <table   className="table is-hoverable is-striped is-fullwidth is-narrow table-container">
-      <MeteoriteListHead />
-        <tbody >
-          {meteorites.map((meteorite, i) => {
-            const year = (typeof meteorite.year !== 'undefined') ? meteorite.year : 'N/A' ;
-            const substr = year.substring(0,4);
-            
-              return <MeteoriteItem 
-              key={i} 
-              name={meteorite.name}
-              fall={meteorite.fall} 
-              geolocation={(typeof meteorite.geolocation !== 'undefined') ? meteorite.geolocation : 'Not applicable'}
-              mass={meteorite.mass} 
-              nametype={meteorite.nametype} 
-              recclass={meteorite.recclass} 
-              reclat={meteorite.reclat} 
-              reclong={meteorite.reclong} 
-              year={substr} 
-            />;
-          })}
-        </tbody>
+    <div style={{ overflowX: "auto" }} className="container">
+      <table className="table is-hoverable is-striped is-fullwidth is-narrow table-container">
+        <MeteoriteListHead />
+        {(meteorites.length === 0 && !displayError ) ? (
+          <tbody><tr><td>Sorry... no results. Try something else.</td></tr></tbody>
+        ) : (
+          <tbody>
+            {meteorites.map((meteorite, i) => {
+              const year =
+                typeof meteorite.year !== "undefined" ? meteorite.year : "N/A";
+              const substr = year.substring(0, 4);
+
+              return (
+                <MeteoriteItem
+                  key={i}
+                  name={meteorite.name}
+                  fall={meteorite.fall}
+                  geolocation={
+                    typeof meteorite.geolocation !== "undefined"
+                      ? meteorite.geolocation
+                      : "Not applicable"
+                  }
+                  mass={meteorite.mass}
+                  nametype={meteorite.nametype}
+                  recclass={meteorite.recclass}
+                  reclat={meteorite.reclat}
+                  reclong={meteorite.reclong}
+                  year={substr}
+                />
+              );
+            })}
+          </tbody>
+        )}
       </table>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Search from './Search';
 
-const Navbar = ({value,onChange}) => {
+const Navbar = ({value,onChange,onSearch}) => {
     return (
-        <nav className="navbar is-warning">
-                <div className="navbar-brand navbar-start">
+        <nav className="navbar is-warning is-flex-touch">
+                <div className="navbar-brand navbar-start is-flex-touch">
                     <a className="navbar-item" href="/">
                         Meteorites
                     </a>
                 </div>
                 <div className="navbar-end">
                     <Search value={value} onChange={onChange} />
+                    <div className="navbar-item">
+                        <button className="button is-primary is-inverted" onClick={onSearch}>Search</button>
+                    </div>
                 </div>
         </nav>
     )
